3525f5b697978b1f0409cbe0dc1c97bef2f6e630
icebergHugger <kolbeinn.floki@gmail.com>
2020-12-23 00:54:51 +0000

    create_board added, cell children put in array

--- knight_move.rb
+++ knight_move.rb
@@ -1,8 +1,24 @@
 class Cell
 
-    def initialize(location, visited=false)
-      @location = location
-      @visited = visited
-    end
+  attr_reader :location, :visited
+
+  attr_writer :children
+
+  def initialize(location, visited=false)
+    @location = location
+    @visited = visited
+    @children = [] #filled with 8 children
+  end
 
 end
+
+def create_board
+  8.times do |col|
+    8.times do |row|
+      cell = Cell.new([col + 1, row + 1])
+    end
+  end
+end
+
+
+create_board
