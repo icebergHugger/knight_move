0b4ddcb97a98247df829cc5f46751337fa9f9c75
icebergHugger <kolbeinn.floki@gmail.com>
2021-01-02 23:36:11 +0000

    Created Board class

--- knight_move_pre.rb
+++ knight_move_pre.rb
@@ -5,5 +5,26 @@ class Node
     @value = value
     @adjacent = []
   end
-  
+
+end
+
+class Board
+  attr_accessor :board
+
+  def initialize(b)
+    @board = chess_board(b)
+  end
+
+  def chess_board(b)
+    arr = Array.new(b) { |index| index + 1}
+
+    board = []
+    arr.each do |i|
+      arr.each do |j|
+        board.push([i, j])
+      end
+    end
+    board
+  end
+
 end
